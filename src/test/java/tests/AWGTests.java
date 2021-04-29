b78051e970eaf655384eadf13eff391144fd3d6b
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-04-30 01:50:39 +0900

    add

--- src/test/java/tests/AWGTests.java
+++ src/test/java/tests/AWGTests.java
@@ -1,6 +1,6 @@
 package tests;
 
-import com.github.javafaker.Faker;
+
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -9,14 +9,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 
-
-public class AWGTests  extends TestBase {
+public class AWGTests extends TestBase {
 
     @Test
     void successfulFillFormTest() {
         step("Open students registration form", () -> {
-            open("https://demoqa.com/automation-practice-form");
-            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            open("https://www.awg.ru/");
+        });
+        step(" Check Service", () -> {
+
+
         });
     }
 }

8b3202ef82fa52e13f3f2f0ff5a7cf13767fd763
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-05-06 02:06:06 +0900

    add

--- src/test/java/tests/AWGTests.java
+++ src/test/java/tests/AWGTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -22,20 +23,36 @@ public class AWGTests extends TestBase {
         step("Open website", () -> {
             open("https://www.awg.ru/");
         });
-       /*
 
-            $(".animate .main-slider__title").shouldHave(text("Увеличим прибыль с омниканальной ИТ трансформацией"));
 
-            */
-             step(" Check Service", () -> {
+        //$(".animate .main-slider__title").shouldHave(text("Увеличим продажи и конверсию с помощью ускорения сайта"));
+
+
+        step(" Check Service", () -> {
             $(byText("Услуги")).click();
-           // $(".page-title__title").shouldHave(text("Услуги"));
+            $(".page-title__title").shouldHave(text("Услуги"));
             $(".services__text").shouldHave(text("Мы ускоряем развитие бизнеса наших клиентов и выводим его на новый уровень с помощью информационных технологий (ИТ). Исходя из целей и проблем бизнеса, мы погружаемся в детали и помогаем найти решения, дающие быстрые и максимально эффективные результаты."));
+        });
+        step(" Check Contacts", () -> {
+            $(byText("Контакты")).click();
+            $(".contacts-info__phone").shouldHave(text("+7 495 278-07-08"));
+            $(".contacts-info__email").shouldHave(text("info@awg.ru"));
         });
         step(" Check Download", () -> {
             Configuration.downloadsFolder = "./downloads";
             $(".footer__download-link").download();
             FileUtils.deleteDirectory(new File("./downloads"));
         });
+        step(" Check language", () -> {
+            $(".header-menu__lang").click();
+            $(".main-services__title_white").shouldHave(text("Popular services,"));
+
+        });
+        step(" Check language", () -> {
+            $(".main-slider__title").shouldHave(text("Увеличим продажи и конверсию с помощью ускорения сайта"));
+        });
     }
 }
+
+
+

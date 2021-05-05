94eb2e3877aef4ab0b977a145a63588476e87a58
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-05-05 15:16:09 +0900

    add

--- src/test/java/tests/AWGTests.java
+++ src/test/java/tests/AWGTests.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.commands.ShouldHave;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -27,7 +28,8 @@ public class AWGTests extends TestBase {
            // $(".page-title__title").shouldHave(text("Услуги"));
             $(".services__text").shouldHave(text("Мы ускоряем развитие бизнеса наших клиентов и выводим его на новый уровень с помощью информационных технологий (ИТ). Исходя из целей и проблем бизнеса, мы погружаемся в детали и помогаем найти решения, дающие быстрые и максимально эффективные результаты."));
         });
-
-
+        step(" Check Download", () -> {
+            $(".footer__download-link").download();
+        });
     }
 }
